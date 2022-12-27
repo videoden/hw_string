38b512b9f6f202e18aaaf82596f868aa4c785c5a
qmar <video4den@gmail.com>
2022-12-27 12:07:46 +0500

    создал ветку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        // task8();
+    }
+
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+
+
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+
     }
 }
